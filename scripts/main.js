2a9adf04fb2c3dfe66f9e12dba9d8903508ee178
LudmylaUsatenko <luda.usatenko00@gmail.com>
2023-11-29 13:39:48 +0200

    added section business , adaptive reviews

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,39 @@
-$(document).ready(function() {
-    
-})
+const slider = $('.reviews__slider');
+
+slider.slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [
+        {
+            breakpoint: 1440,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+            }
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 640,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+
+    ]
+});
